e0d27fb5be8e91f6dd8961cc374f344fd5c598fe
Daniele Costa <santoscostadaniele.com>
2026-02-02 19:14:17 -0300

    UI - gitignore

--- README.md
+++ README.md
@@ -8,19 +8,33 @@ plataforma Udemy </strong>, utilizando <strong> Angular </strong> no frontend e 
    - <p> <strong> Plataforma: </strong> Udemy </p>
    - <p> <strong> Professor: </strong> Sameer Saini </p>
 
+### repository
+
+`echo "# ANGULAR20-ASP.NETCoreWebAPI-" >> README.md`
+`git init`
+`git add README.md`
+`git commit -m "first commit"`
+`git branch -M main`
+`git remote add origin [link]`
+`git push -u origin main`
+
 ### push
 
 `git remote add origin [link]` <br>
 `git branch -M main` <br>
 `git push -u origin main` <br>
 
-echo "# ANGULAR20-ASP.NETCoreWebAPI-" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin [link]
-git push -u origin main
+<p>Ligar servidor</p>
+
+`ng serve`
+
+<p>Criar componentes</p>
+
+`ng g c navbar`
+
+<p> Criar service</p>
+
+`ng g s [servicename]`
 
 
 	
---
+++ UI/CodePulse/src/app/features/category/add-category/add-category.ts
@@ -0,0 +1,62 @@
+import { Component, effect, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AddCategoryRequest } from '../models/category.model';
+import { CategoryService } from '../services/category-service';
+
+@Component({
+  selector: 'app-add-category',
+  imports: [ReactiveFormsModule],
+  templateUrl: './add-category.html',
+  styleUrl: './add-category.css',
+})
+
+export class AddCategory {
+
+  constructor() {
+    effect(() => {
+      if (this.categoryService.addCategoryStatus() === 'success') {
+        console.error('Success');
+      }
+
+      if (this.categoryService.addCategoryStatus() === 'error') {
+        console.error('Add Category Request Failed');
+      }
+    });
+  }
+  
+  private categoryService = inject(CategoryService);
+  addCategoryFormGroup = new FormGroup({
+    name: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.maxLength(100)],
+    }),
+    urlHandle: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.maxLength(200)],
+    }),
+  });
+
+  get nameFormControl() {
+    // return this.addCategoryFormGroup.get('name');
+    return this.addCategoryFormGroup.controls.name;
+  }
+
+  get urlHandleFormControl() {
+    // return this.addCategoryFormGroup.get('urlHandle');
+    return this.addCategoryFormGroup.controls.urlHandle;
+  }
+
+  onSubmit() {
+    const addCategoryFormValue = this.addCategoryFormGroup.getRawValue();
+
+    const AddCategoryRequestDto: AddCategoryRequest = {
+      name: addCategoryFormValue.name,
+      urlHandle: addCategoryFormValue.urlHandle,
+    };
+
+    this.categoryService.addCategory(AddCategoryRequestDto);
+
+
+
+  }
+}
